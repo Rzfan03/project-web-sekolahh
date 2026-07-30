3146307a1d4ff87809811248fb8abf2f785d65fa
rzfann <fajrulsyaban47@gmail.com>
2026-07-30 07:40:06 +0700

    feat: add navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,16 @@ import { FaChevronDown } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-amber-500 p-5 h-fit">
+    <div className="w-full bg-orange-400 h-fit p-5">
 <div className="flex justify-around items-center">
   <img className="h-24 w-25" src="https://smkn1sumbawa.sch.id/wp-content/uploads/2022/08/Logo-SMKN-1.png"/>
   <ul className="flex items-center text-lg gap-7 text-white font-semibold">
     <div className="group relative cursor-pointer">
       <a className="">Beranda</a>
       <div className="group-hover:w-full w-0 transition-all bg-white h-0.5 absolute"></div>
+      <div cl>
+        
+      </div>
     </div>
     <div className="group relative cursor-pointer">
       <a className="flex items-center gap-2">Profil<FaChevronDown className="group-hover:rotate-180 transition-all"/></a>
